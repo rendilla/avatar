4fd9f345e1c94b249c977012ec39060cfc24f050
rendilla <reynlouise.padilla@sjsu.edu>
2021-04-01 20:53:58 -0700

    2.1

--- sketch.js
+++ sketch.js
@@ -1,12 +1,52 @@
 //types of houseplants
-let plants = ["anthurium", "philodendron", "calatheas", "pothos", "peperomia" ];
+let plants = [{
+  name: "anthurium",
+  color: "black king"
+}, {
+  name: "philodendron",
+  color: "green congo"
+}, {
+  name: "calathea",
+  color: "white fusion"
+}, {
+  name: "pothos",
+  color: "grey feather"
+}, {
+  name: "peperomia",
+  color: "striped one"
+}, {
+  name: "hoya",
+  color: "murky splash"
+}];
+// plants[1] = "philodendron leaf"
+// console.log(plants);
+//
+// console.log("initial array is " + plants)
+// console.log(plants);
+//
+// plants.splice(2, 2);
+// console.log("array after splice")
+// console.log(plants);
 
-console.log(plants[0]);
+
+let randomIndex;
 
 function setup() {
   createCanvas(400, 400);
+  background(220);
+
+// pull a random integer from objects:
+// console.log(random(5));
+// console.log(plants[int(random(5))]);
+
 }
 
 function draw() {
-  background(220);
+}
+
+function mousePressed() {
+  background(random(150,200));
+  randomIndex= int(random(plants.length));
+  text(plants[randomIndex].name, 50,50);
+  plants.splice(randomIndex, 1);
 }
